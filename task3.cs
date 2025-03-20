f35765f955a345e2dc86ed545c48e260b21a3096
movsesyan2002 <dav.movsesyan44@gmail.com>
2025-03-20 20:28:29 +0400

    Update task3.cs

--- task3.cs
+++ task3.cs
@@ -4,39 +4,39 @@
 // A read-only property FullName that is computed only once when accessed for the first time (lazy initialization).
 // Ensure FullName is stored internally and does not change if FirstName or LastName are modified.
 
-// class Person {
+class Person {
 
-//     public string firstName { get; set;}
-//     public string lastName { get; set;}
-//     private string _fullname;
-//     private bool _iscomplete = false;
+    public string firstName { get; set;}
+    public string lastName { get; set;}
+    private string _fullname;
+    private bool _iscomplete = false;
 
-//     public Person(string FirstName,string LastName) {
-//         this.firstName = FirstName;
-//         this.lastName = LastName;
-//     }
+    public Person(string FirstName,string LastName) {
+        this.firstName = FirstName;
+        this.lastName = LastName;
+    }
 
-//     public string FullName { 
-//         get{
-//             if (this._iscomplete == false)
-//             {
-//                 _iscomplete = true;
-//                 _fullname = $"{this.firstName} {this.lastName}";
-//             }
-//             return this._fullname;
-//         }
-//     }
+    public string FullName { 
+        get{
+            if (this._iscomplete == false)
+            {
+                _iscomplete = true;
+                _fullname = $"{this.firstName} {this.lastName}";
+            }
+            return this._fullname;
+        }
+    }
 
-// }
+}
 
-// class Program {
-//     static void Main(string[] args) {
-//         Person person = new Person("Dav", "Movsesyan");
-//         Console.WriteLine($"{person.FullName}");
-//         person.firstName = "Narek";
-//         Console.WriteLine($"{person.firstName} {person.lastName}");
-//         Console.WriteLine($"{person.FullName}");
+class Program {
+    static void Main(string[] args) {
+        Person person = new Person("Dav", "Movsesyan");
+        Console.WriteLine($"{person.FullName}");
+        person.firstName = "Narek";
+        Console.WriteLine($"{person.firstName} {person.lastName}");
+        Console.WriteLine($"{person.FullName}");
 
-//     }
-// }
+    }
+}
 
