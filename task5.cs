c4688a9b62fa2ba54b4f1dee4cb83cee0ce66bae
movsesyan2002 <dav.movsesyan44@gmail.com>
2025-03-20 20:29:09 +0400

    Update task5.cs

--- task5.cs
+++ task5.cs
@@ -3,42 +3,42 @@
 // A property TotalSeconds (int).
 // A computed property FormattedTime that returns "HH:mm:ss" format.
 
-// class TimePeriod {
+class TimePeriod {
 
-//     private int _second;
+    private int _second;
 
-//     public int TotalSeconds {
-//         get { return _second; }
+    public int TotalSeconds {
+        get { return _second; }
 
-//         set {
-//             if (value < 0) {
-//                 throw new ArgumentOutOfRangeException("The second is negative");
-//             }
-//             else {
-//                 _second = value;
-//             }
-//         }
-//     }
+        set {
+            if (value < 0) {
+                throw new ArgumentOutOfRangeException("The second is negative");
+            }
+            else {
+                _second = value;
+            }
+        }
+    }
 
-//     public string FormattedTime {
-//         get { return Convert(_second); }
-//     }
+    public string FormattedTime {
+        get { return Convert(_second); }
+    }
 
-//     public string Convert(int seconds) {
-//         int hour = seconds / 3600;
-//         seconds%=3600;
-//         int minute = seconds / 60;
-//         seconds%=60;
-//         return $"{hour}:{minute}:{seconds}";
-//     }
+    public string Convert(int seconds) {
+        int hour = seconds / 3600;
+        seconds%=3600;
+        int minute = seconds / 60;
+        seconds%=60;
+        return $"{hour}:{minute}:{seconds}";
+    }
 
-// }
+}
 
-// class Program {
-//     static void Main(string[] args) {
-//         TimePeriod timePeriod = new TimePeriod();
-//         timePeriod.TotalSeconds = 3800;
-//         Console.WriteLine($"Formatted is {timePeriod.FormattedTime}");
-//     }
-// }
+class Program {
+    static void Main(string[] args) {
+        TimePeriod timePeriod = new TimePeriod();
+        timePeriod.TotalSeconds = 3800;
+        Console.WriteLine($"Formatted is {timePeriod.FormattedTime}");
+    }
+}
 
